4e4f8ed0bb32252392eabd017f4925d0b9c930f6
e6ntff <pashatar2@gmail.com>
2024-02-08 19:05:16 +0300

    added adding to cart

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,38 @@
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import { observer } from 'mobx-react-lite';
+import React, { useMemo } from 'react';
+import store from '../utils/store';
+import { Item, ItemInCart } from '../utils/interfaces';
+
+interface Props {
+	id: number;
+}
+
+const AddToCartButton: React.FC<Props> = observer(({ id }) => {
+	const { addItemToCart, removeItemFromCart, cart } = store;
+
+	const isItemInCart = useMemo(
+		() => cart.some((el: ItemInCart) => el.item.id === id),
+		[cart]
+	);
+
+	const toggleItemInCart = () => {
+		if (isItemInCart) {
+			removeItemFromCart(id);
+		} else {
+			addItemToCart(id);
+		}
+	};
+
+	return (
+		<Button
+			onClick={toggleItemInCart}
+			type={isItemInCart ? 'primary' : 'default'}
+		>
+			<ShoppingCartOutlined />
+		</Button>
+	);
+});
+
+export default AddToCartButton;
