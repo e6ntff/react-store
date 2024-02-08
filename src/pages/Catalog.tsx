92d1fe7ecef18806e53027f65b8bf78b8d037380
e6ntff <pashatar2@gmail.com>
2024-02-08 19:09:59 +0300

    added catalog

---
+++ src/pages/Catalog.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import CardsList from '../components/CardsList';
+import CategoriesNavigation from '../components/CategoriesNavigation';
+import SearchBar from '../components/SearchBar';
+import { observer } from 'mobx-react-lite';
+import styled from 'styled-components';
+import CartButton from '../components/CartButton';
+
+const HeaderWrapper = styled.div`
+	display: flex;
+	gap: 1.5rem;
+	justify-content: space-between;
+`;
+
+const Catalog: React.FC = observer(() => {
+	return (
+		<>
+			<HeaderWrapper>
+				<SearchBar />
+				<CartButton />
+			</HeaderWrapper>
+			<CategoriesNavigation />
+			<CardsList />
+		</>
+	);
+});
+
+export default Catalog;
