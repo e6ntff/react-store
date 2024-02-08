e21b818fbcb278b1f974da0bf5f6d4e77c084506
e6ntff <pashatar2@gmail.com>
2024-02-08 21:15:15 +0300

    fixed cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,20 @@
-import { Image, List } from 'antd';
+import { Descriptions, Empty, Image, List } from 'antd';
 import Item from 'antd/es/list/Item';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import store from '../utils/store';
 import { ItemInCart } from '../utils/interfaces';
-import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
+import { DeleteOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import CatalogButton from '../components/CatalogButton';
+import OrderButton from '../components/OrderButton';
 
 const Cart: React.FC = observer(() => {
-	const { cart, getItemInCartQuantity, changeItemInCartQuantity } = store;
+	const {
+		cart,
+		getItemInCartQuantity,
+		changeItemInCartQuantity,
+		removeItemFromCart,
+	} = store;
 
 	return (
 		<>
@@ -17,30 +23,36 @@ const Cart: React.FC = observer(() => {
 				size='small'
 				itemLayout='vertical'
 			>
-				{cart.map(({ item }: ItemInCart) => (
-					<Item
-						key={item.id}
-						actions={[
-							<MinusOutlined
-								onClick={() => changeItemInCartQuantity(item.id, -1)}
-							/>,
-							<span>{getItemInCartQuantity(item.id)}</span>,
-							<PlusOutlined
-								onClick={() => changeItemInCartQuantity(item.id, 1)}
-							/>,
-						]}
-						extra={
-							<Image
-								style={{ blockSize: '10rem' }}
-								src={item.image}
-							/>
-						}
-						style={{ fontWeight: 700 }}
-					>
-						{item.title}
-					</Item>
-				))}
+				{cart.length ? (
+					cart.map(({ item }: ItemInCart) => (
+						<Item
+							key={item.id}
+							actions={[
+								<MinusOutlined
+									onClick={() => changeItemInCartQuantity(item.id, -1)}
+								/>,
+								<span>{getItemInCartQuantity(item.id)}</span>,
+								<PlusOutlined
+									onClick={() => changeItemInCartQuantity(item.id, 1)}
+								/>,
+								<DeleteOutlined onClick={() => removeItemFromCart(item.id)} />,
+							]}
+							extra={
+								<Image
+									style={{ blockSize: '5rem' }}
+									src={item.image}
+								/>
+							}
+							style={{ fontWeight: 700 }}
+						>
+							{item.title}
+						</Item>
+					))
+				) : (
+					<Empty description='No items in cart' />
+				)}
 			</List>
+			{cart.length ? <OrderButton /> : <></>}
 		</>
 	);
 });
