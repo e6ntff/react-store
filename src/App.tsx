c5b06c1b0b320eb35fd3ad0697d3642f5bb61633
e6ntff <pashatar2@gmail.com>
2024-02-08 19:09:51 +0300

    added routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { HashRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
+import { observer } from 'mobx-react-lite';
+import store from './utils/store';
 
-function App() {
-	return <></>;
-}
+const App: React.FC = observer(() => {
+	const { setCart } = store;
+
+	useEffect(() => {
+		const cartFromStorage = localStorage.getItem('cart');
+
+		if (cartFromStorage) {
+			try {
+				const parsedCart = JSON.parse(cartFromStorage);
+				setCart(parsedCart);
+			} catch (error) {
+				console.error('Error parsing JSON:', error);
+			}
+		}
+	}, [setCart]);
+
+	return (
+		<HashRouter>
+			<AppRouter />
+		</HashRouter>
+	);
+});
 
 export default App;
