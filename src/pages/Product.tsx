bd32d9caa549e8eaae9b2d2bfd8358d1442d2e00
e6ntff <pashatar2@gmail.com>
2024-02-09 18:47:04 +0300

    added product loading

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,55 +1,74 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { Col, Flex, Image, List, Rate, Row, Tag, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Col, Flex, Image, Rate, Row, Spin, Tag, Typography } from 'antd';
 import CatalogButton from '../components/CatalogButton';
 import Title from 'antd/es/typography/Title';
-import Item from 'antd/es/list/Item';
 import AddToCartButton from '../components/AddToCartButton';
 import { itemsStore } from '../utils/store';
+import { Item } from '../utils/interfaces';
+import getItem from '../utils/getItem';
 
 const Product: React.FC = observer(() => {
-	const { currentItem } = itemsStore;
-	const { id, title, price, description, category, image, rating } =
-		currentItem;
+	const { currentItemId, items, isItemLoaded, setIsItemLoaded } = itemsStore;
+	const [currentItem, setCurrentItem] = useState<Item>(items[0]);
+
+	useEffect(() => {
+		getItem(currentItemId).then((data: Item) => {
+			setCurrentItem(data);
+			setIsItemLoaded(true);
+		});
+		return () => {
+			setIsItemLoaded(false);
+		};
+	}, [setCurrentItem, setIsItemLoaded, currentItemId]);
+
 	return (
 		<>
 			<CatalogButton />
-			<Row gutter={[48, 48]}>
-				<Col span={12}>
-					<Image
-						src={image}
-						style={{ inlineSize: '20rem' }}
-					/>
-				</Col>
-				<Col span={12}>
-					<Flex
-						vertical
-						align='stretch'
-					>
-						<Title level={1}>{title}</Title>
-						<Flex justify='space-between'>
-							<Tag bordered>{category}</Tag>
-							<Rate
-								disabled
-								value={rating.rate}
-								allowClear
-								allowHalf
+			{isItemLoaded ? (
+				<>
+					<Row gutter={[48, 48]}>
+						<Col span={12}>
+							<Image
+								src={currentItem.image}
+								style={{ inlineSize: '20rem' }}
 							/>
-						</Flex>
-						<Flex
-							justify='space-between'
-							align='center'
-						>
-							<AddToCartButton id={id} />
-							<Title level={2}>${price}</Title>
-						</Flex>
-						<Typography.Text type='secondary'>{description}</Typography.Text>
-					</Flex>
-				</Col>
-			</Row>
-			<List>
-				<Item></Item>
-			</List>
+						</Col>
+						<Col span={12}>
+							<Flex
+								vertical
+								align='stretch'
+							>
+								<Title level={1}>{currentItem.title}</Title>
+								<Flex justify='space-between'>
+									<Tag bordered>{currentItem.category}</Tag>
+									<Rate
+										disabled
+										value={currentItem.rating.rate}
+										allowClear
+										allowHalf
+									/>
+								</Flex>
+								<Flex
+									justify='space-between'
+									align='center'
+								>
+									<AddToCartButton id={currentItem.id} />
+									<Title level={2}>${currentItem.price}</Title>
+								</Flex>
+								<Typography.Text type='secondary'>
+									{currentItem.description}
+								</Typography.Text>
+							</Flex>
+						</Col>
+					</Row>
+				</>
+			) : (
+				<Spin
+					size='large'
+					fullscreen
+				/>
+			)}
 		</>
 	);
 });
