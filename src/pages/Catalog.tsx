bf3751160e860dcc8c9123c9f5df1f9480d0152f
e6ntff <pashatar2@gmail.com>
2024-02-09 18:46:26 +0300

    fixed catalog styles

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -3,22 +3,16 @@ import CardsList from '../components/CardsList';
 import CategoriesNavigation from '../components/CategoriesNavigation';
 import SearchBar from '../components/SearchBar';
 import { observer } from 'mobx-react-lite';
-import styled from 'styled-components';
 import CartButton from '../components/CartButton';
-
-const HeaderWrapper = styled.div`
-	display: flex;
-	gap: 1.5rem;
-	justify-content: space-between;
-`;
+import { Flex } from 'antd';
 
 const Catalog: React.FC = observer(() => {
 	return (
 		<>
-			<HeaderWrapper>
+			<Flex gap={16}>
 				<SearchBar />
 				<CartButton />
-			</HeaderWrapper>
+			</Flex>
 			<CategoriesNavigation />
 			<CardsList />
 		</>
